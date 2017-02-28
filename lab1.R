2f78b0c7332cfc3ceae64149f443eb7eb6dd49d2
YongGY <715guoyong@gmail.com>
2017-02-28 16:41:55 -0600

    lab1

---
+++ lab1.R
@@ -0,0 +1,13 @@
+x=c(1,2,3,4,5)
+y=c(1,2,1.3,3.75,2.25)
+sx=sum(x)
+sy=sum(y)
+plot(x,y,type='o')
+meanx=mean(x)
+meany=mean(y)
+sumxy=sum(x*y)
+sumxx=sum(x*x)
+sumyy=sum(y*y)
+r=sumxy/sqrt(sumxx*sumyy)
+m=r*sy/sx
+c=meany-m*meanx
